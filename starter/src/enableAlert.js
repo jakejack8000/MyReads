24a93f5289a9bcacd16d8f984925544d23ed2640
samuel30002 <samuelkollex99@gmail.com>
2022-09-08 02:16:49 +0200

    init

--- starter/src/enableAlert.js
+++
@@ -1,7 +0,0 @@
-const EnableAlert = ({enableAlert,setEnableAlert}) => {
-    return <button className='enable-alert-button' onClick={()=> {setEnableAlert(!enableAlert)}}>
-        {enableAlert ? 'Disable Alert after move':'Enable Alert after move'}
-    </button>
-}
-
-export default EnableAlert
